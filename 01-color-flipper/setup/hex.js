b999dee6f6748887d61d15dcffba8f8f15886e40
acst352 <acst352@gmail.com>
2023-09-06 23:36:29 -0600

    Create hex.js code

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hexValues = "0123456789ABCDEF";
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const hexColor = generateRandomHexColor();
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function generateRandomHexColor() {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * hexValues.length);
+    hexColor += hexValues[randomIndex];
+  }
+  return hexColor;
+}
