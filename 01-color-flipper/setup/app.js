aaa93bad7996f1a74e81051469065e299a54fada
acst352 <acst352@gmail.com>
2023-09-06 23:29:25 -0600

    Create app.js code
    Allow user to change background color

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const randomNumber = getRandomNumber();
+  const selectedColor = colors[randomNumber];
+
+  document.body.style.backgroundColor = selectedColor;
+
+  color.textContent = selectedColor;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
